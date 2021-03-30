ddf7c74a88f20ad62ef8887d999b9b62b033d26a
Ebubekir Sezer <esezer@protel.com.tr>
2021-03-30 20:35:37 +0300

    ADDED: Fotoğraf çekme ve seçme işlemleri eklendi

---
+++ AlphaLogi/AlphaLogi/Views/HomePage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AlphaLogi.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonImage_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Please pick a photo"
+                });
+
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        async void takeImage_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                var result = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Please take a photo"
+                });
+
+                var stream = await result.OpenReadAsync();
+
+                if (ImageSource.FromStream(() => stream) != null)
+                {
+                    resultImage.Source = ImageSource.FromStream(() => stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            
+        }
+    }
+}
